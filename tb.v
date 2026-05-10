171627ce471322b2e051666654ad0178a3e11483
himanigoel0 <24110134@iitgn.ac.in>
2026-05-10 17:00:05 +0530

    Update tb.v

--- tb.v
+++ tb.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05/07/2026 04:21:05 PM
-// Design Name: 
-// Module Name: tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module tb #(
     parameter M = 32,
